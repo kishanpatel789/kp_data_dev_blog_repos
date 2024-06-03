470e3ade2fd62f0a2e951ca26cdfd3586f095cdd
Kishan Patel <mail@kishanpatel.dev>
2024-06-03 16:05:58 -0500

    Update to Hogwarts scene

--- snowflake_with_terraform/modules/snowflake/main.tf
+++ snowflake_with_terraform/modules/snowflake/main.tf
@@ -12,165 +12,165 @@ provider "snowflake" {
 }
 
 # database, schemas, warehouse
-resource "snowflake_database" "letterboxd_db" {
-  name                        = "LETTERBOXD"
+resource "snowflake_database" "hogwarts_db" {
+  name                        = "HOGWARTS"
   data_retention_time_in_days = 0
 }
 
 resource "snowflake_schema" "raw" {
-  database            = snowflake_database.letterboxd_db.name
+  database            = snowflake_database.hogwarts_db.name
   name                = "RAW"
   data_retention_days = 0
 }
 
 resource "snowflake_schema" "staged" {
-  database            = snowflake_database.letterboxd_db.name
+  database            = snowflake_database.hogwarts_db.name
   name                = "STAGED"
   data_retention_days = 0
 }
 
 resource "snowflake_schema" "main" {
-  database            = snowflake_database.letterboxd_db.name
+  database            = snowflake_database.hogwarts_db.name
   name                = "MAIN"
   data_retention_days = 0
 }
 
-resource "snowflake_warehouse" "letterboxd_wh" {
-  name           = "LETTERBOXD_WH"
+resource "snowflake_warehouse" "hogwarts_wh" {
+  name           = "HOGWARTS_WH"
   warehouse_type = "STANDARD"
   warehouse_size = "x-small"
   auto_resume    = true
-  auto_suspend   = 1200
+  auto_suspend   = 600
 }
 
 # user and role
-resource "snowflake_user" "svc_letterboxd" {
-  name              = "SVC_LETTERBOXD"
+resource "snowflake_user" "svc_hogwarts" {
+  name              = "SVC_HOGWARTS"
   password          = var.service_user_password
-  default_warehouse = snowflake_warehouse.letterboxd_wh.name
-  default_role      = snowflake_role.svc_letterboxd_role.name
-  default_namespace = snowflake_database.letterboxd_db.name
+  default_warehouse = snowflake_warehouse.hogwarts_wh.name
+  default_role      = snowflake_role.svc_hogwarts_role.name
+  default_namespace = snowflake_database.hogwarts_db.name
 }
 
-resource "snowflake_role" "svc_letterboxd_role" {
-  name = "SVC_LETTERBOXD_ROLE"
+resource "snowflake_role" "svc_hogwarts_role" {
+  name = "SVC_HOGWARTS_ROLE"
 }
 
 resource "snowflake_grant_account_role" "grant_role" {
-  role_name = snowflake_role.svc_letterboxd_role.name
-  user_name = snowflake_user.svc_letterboxd.name
+  role_name = snowflake_role.svc_hogwarts_role.name
+  user_name = snowflake_user.svc_hogwarts.name
 }
 
 # service role privileges
 resource "snowflake_grant_privileges_to_account_role" "db_usage" {
   privileges        = ["USAGE"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_account_object {
     object_type = "DATABASE"
-    object_name = snowflake_database.letterboxd_db.name
+    object_name = snowflake_database.hogwarts_db.name
   }
 }
 
 resource "snowflake_grant_privileges_to_account_role" "schema_privileges_raw" {
   privileges        = ["USAGE", "MODIFY", "CREATE TABLE", "CREATE VIEW"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_schema {
-    schema_name = "\"${snowflake_database.letterboxd_db.name}\".\"${snowflake_schema.raw.name}\""
+    schema_name = "\"${snowflake_database.hogwarts_db.name}\".\"${snowflake_schema.raw.name}\""
   }
 }
 
 resource "snowflake_grant_privileges_to_account_role" "schema_privileges_staged" {
   privileges        = ["USAGE", "MODIFY", "CREATE TABLE", "CREATE VIEW"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_schema {
-    schema_name = "\"${snowflake_database.letterboxd_db.name}\".\"${snowflake_schema.staged.name}\""
+    schema_name = "\"${snowflake_database.hogwarts_db.name}\".\"${snowflake_schema.staged.name}\""
   }
 }
 
 resource "snowflake_grant_privileges_to_account_role" "schema_privileges_main" {
   privileges        = ["USAGE", "MODIFY", "CREATE TABLE", "CREATE VIEW"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_schema {
-    schema_name = "\"${snowflake_database.letterboxd_db.name}\".\"${snowflake_schema.main.name}\""
+    schema_name = "\"${snowflake_database.hogwarts_db.name}\".\"${snowflake_schema.main.name}\""
   }
 }
 
 resource "snowflake_grant_privileges_to_account_role" "table_privileges_raw" {
   privileges        = ["SELECT", "INSERT", "UPDATE", "DELETE"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_schema_object {
     all {
       object_type_plural = "TABLES"
-      in_schema          = "\"${snowflake_database.letterboxd_db.name}\".\"${snowflake_schema.raw.name}\""
+      in_schema          = "\"${snowflake_database.hogwarts_db.name}\".\"${snowflake_schema.raw.name}\""
     }
   }
 }
 
 resource "snowflake_grant_privileges_to_account_role" "future_table_privileges_raw" {
   privileges        = ["SELECT", "INSERT", "UPDATE", "DELETE"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_schema_object {
     future {
       object_type_plural = "TABLES"
-      in_schema          = "\"${snowflake_database.letterboxd_db.name}\".\"${snowflake_schema.raw.name}\""
+      in_schema          = "\"${snowflake_database.hogwarts_db.name}\".\"${snowflake_schema.raw.name}\""
     }
   }
 }
 
 resource "snowflake_grant_privileges_to_account_role" "table_privileges_staged" {
   privileges        = ["SELECT", "INSERT", "UPDATE", "DELETE"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_schema_object {
     all {
       object_type_plural = "TABLES"
-      in_schema          = "\"${snowflake_database.letterboxd_db.name}\".\"${snowflake_schema.staged.name}\""
+      in_schema          = "\"${snowflake_database.hogwarts_db.name}\".\"${snowflake_schema.staged.name}\""
     }
   }
 }
 
 resource "snowflake_grant_privileges_to_account_role" "future_table_privileges_staged" {
   privileges        = ["SELECT", "INSERT", "UPDATE", "DELETE"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_schema_object {
     future {
       object_type_plural = "TABLES"
-      in_schema          = "\"${snowflake_database.letterboxd_db.name}\".\"${snowflake_schema.staged.name}\""
+      in_schema          = "\"${snowflake_database.hogwarts_db.name}\".\"${snowflake_schema.staged.name}\""
     }
   }
 }
 
 resource "snowflake_grant_privileges_to_account_role" "table_privileges_main" {
   privileges        = ["SELECT", "INSERT", "UPDATE", "DELETE"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_schema_object {
     all {
       object_type_plural = "TABLES"
-      in_schema          = "\"${snowflake_database.letterboxd_db.name}\".\"${snowflake_schema.main.name}\""
+      in_schema          = "\"${snowflake_database.hogwarts_db.name}\".\"${snowflake_schema.main.name}\""
     }
   }
 }
 
 resource "snowflake_grant_privileges_to_account_role" "future_table_privileges_main" {
   privileges        = ["SELECT", "INSERT", "UPDATE", "DELETE"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_schema_object {
     future {
       object_type_plural = "TABLES"
-      in_schema          = "\"${snowflake_database.letterboxd_db.name}\".\"${snowflake_schema.main.name}\""
+      in_schema          = "\"${snowflake_database.hogwarts_db.name}\".\"${snowflake_schema.main.name}\""
     }
   }
 }
 
 resource "snowflake_grant_privileges_to_account_role" "warehouse_privileges" {
   privileges        = ["USAGE"]
-  account_role_name = snowflake_role.svc_letterboxd_role.name
+  account_role_name = snowflake_role.svc_hogwarts_role.name
   on_account_object {
     object_type = "WAREHOUSE"
-    object_name = snowflake_warehouse.letterboxd_wh.name
+    object_name = snowflake_warehouse.hogwarts_wh.name
   }
 }
 
 resource "snowflake_grant_account_role" "svc_to_sysadmin" {
-  role_name        = snowflake_role.svc_letterboxd_role.name
+  role_name        = snowflake_role.svc_hogwarts_role.name
   parent_role_name = "SYSADMIN"
 }
--- snowflake_with_terraform/modules/snowflake/outputs.tf
+++ snowflake_with_terraform/modules/snowflake/outputs.tf
@@ -1,7 +1,7 @@
 output "snowflake_warehouse_name" {
-  value = snowflake_warehouse.letterboxd_wh.name
+  value = snowflake_warehouse.hogwarts_wh.name
 }
 
 output "snowflake_service_user_username" {
-  value = snowflake_user.svc_letterboxd.name
+  value = snowflake_user.svc_hogwarts.name
 }
