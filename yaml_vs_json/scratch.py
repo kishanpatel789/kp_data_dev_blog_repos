7af297ba5b564a4f4e9bcdb07dd1652727bd5543
Kishan Patel <mail@kishanpatel.dev>
2025-03-25 16:56:15 -0500

    Update samples

--- yaml_vs_json/scratch.py
+++ yaml_vs_json/scratch.py
@@ -8,3 +8,6 @@ with open("./config.json", "r") as f:
 with open("./config.yaml", "r") as f:
     config_yaml = yaml.safe_load(f)
 # %%
+with open("./config2.yaml", "r") as f:
+    config2_yaml = yaml.safe_load(f)
+# %%
