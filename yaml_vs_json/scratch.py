2f8fcb803ebe0f2292041b5f6c4aef99f0cd055b
Kishan Patel <mail@kishanpatel.dev>
2025-03-27 14:28:14 -0500

    Rename config files

--- yaml_vs_json/scratch.py
+++ yaml_vs_json/scratch.py
@@ -1,15 +1,23 @@
-#%%
+# %%
 import json
 import yaml
-# %%
-with open("./config.json", "r") as f:
-    config_json = json.load(f)
 
-with open("./config.yaml", "r") as f:
-    config_yaml = yaml.safe_load(f)
+def read_config_files(suffix: str) -> tuple[dict, dict]:
+    """
+    Read JSON and YAML configuration files.
+    Args:
+        suffix (str): Suffix of file names.
+    Returns:    
+        tuple[dict, dict]: Tuple containing the JSON and YAML configurations as dictionaries.
+    """
+
+    with open(f"./config_{suffix}.json", "r") as f:
+        config_json = json.load(f)
+    with open(f"./config_{suffix}.yaml", "r") as f:
+        config_yaml = yaml.safe_load(f)
+    return config_json, config_yaml
+
 # %%
-with open("./config2.yaml", "r") as f:
-    config2_yaml = yaml.safe_load(f)
-# %%
-with open("./config3.yaml", "r") as f:
-    config3_yaml = yaml.safe_load(f)
+intro_json, intro_yaml = read_config_files("intro")
+repeated_json, repeated_yaml = read_config_files("repeated")
+# long_string_json, long_string_yaml = read_config_files("long_string")
