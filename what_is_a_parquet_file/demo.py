59830e97c1f4efaecd2bb99e3e701411d3451af8
Kishan Patel <kishanpatel789@gmail.com>
2025-08-14 15:37:31 -0500

    Start presentation

---
+++ what_is_a_parquet_file/demo.py
@@ -0,0 +1,119 @@
+# %%
+from datetime import datetime
+import pyarrow as pa
+import pyarrow.parquet as pq
+import random
+
+# %%
+
+file_path = "data/yellow_tripdata_2025-05.parquet"
+
+# %%
+table = pq.read_table(file_path)
+table.schema
+table.shape
+
+table
+
+# %%
+parquet_file = pq.ParquetFile(file_path)
+parquet_file.metadata
+parquet_file.schema
+
+parquet_file.num_row_groups
+
+parquet_file.read_row_group(0)
+
+parquet_file.read()
+
+
+# %%
+metadata = parquet_file.metadata
+metadata.row_group(0)
+
+for i in range(metadata.num_row_groups):
+    print(f"{i}: {metadata.row_group(i).num_rows}")
+
+metadata.row_group(0).column(0)
+
+# %%
+# save as csv
+df = parquet_file.read().to_pandas()
+
+df.shape
+
+df.to_csv("data/yellow_tripdata_2025-05.csv", index=False)
+
+# du -h data/yellow_tripdata_2025-05*
+# du -h data/*
+
+# %%
+numbers = [random.randint(1, 1000) for _ in range(1_000_000)]
+numbers_table = pa.table({"numbers": numbers})
+
+# %%
+numbers_table
+
+numbers_table.to_pandas()["numbers"].value_counts()
+
+# %%
+# plain encoding
+pq.write_table(
+    numbers_table,
+    "data/numbers_plain.parquet",
+    use_dictionary=False,
+    column_encoding={
+        "numbers": "PLAIN",
+    },
+)
+
+# dictionary encoding
+pq.write_table(numbers_table, "data/numbers_dict.parquet", use_dictionary=True)
+
+# %%
+# run length_encoding
+sorted_table = pa.table({"numbers": sorted(numbers)})
+sorted_table
+
+pq.write_table(sorted_table, "data/numbers_sorted.parquet")
+
+# %%
+# check plain encoding, dictionary encoding
+plain_file = pq.ParquetFile("data/numbers_plain.parquet")
+dict_file = pq.ParquetFile("data/numbers_dict.parquet")
+sorted_file = pq.ParquetFile("data/numbers_sorted.parquet")
+
+# %%
+plain_file.metadata.row_group(0).column(0)
+dict_file.metadata.row_group(0).column(0)
+sorted_file.metadata.row_group(0).column(0)
+
+
+# %%
+# delta encoding
+timestamps = [datetime.now() for _ in range(1_000_000)]
+ts_table = pa.table({"timestamps": timestamps})
+ts_table
+
+# %%
+pq.write_table(
+    ts_table,
+    "data/timestamps_plain.parquet",
+    compression=None,
+    use_dictionary=False,
+    column_encoding={
+        "timestamps": "PLAIN",
+    },
+)
+
+# %%
+pq.write_table(
+    ts_table,
+    "data/timestamps_delta.parquet",
+    compression=None,
+    use_dictionary=False,
+    column_encoding={
+        "timestamps": "DELTA_BINARY_PACKED",
+    },
+)
+
---
+++ what_is_a_parquet_file/presentation/dist/d3_parquet_storage.js
@@ -0,0 +1,227 @@
+const config = {
+  tableRows: 4,
+  tableCols: 3,
+  cellSize: 50,
+  spacing: 10,
+  rowGroupSize: 2,
+
+  tableStartX: 100,
+  tableStartY: 50,
+
+  storageStartX: 400,
+  storageStartY: 50,
+  storageWrap: 7,  // how many cells per row in wrapped layout
+
+  colors: d3.schemeTableau10
+};
+
+config.tableWidth = config.tableCols * config.cellSize + (config.tableCols - 1) * config.spacing;
+config.storageWidth = config.storageWrap * (config.cellSize + config.spacing);
+config.cellsPerRG = config.rowGroupSize * config.tableCols;
+config.numRG = Math.ceil(config.tableRows / config.rowGroupSize);
+
+
+function writeSectionHeaders(svg, storage_title) {
+  const headerOffset = 20
+
+  svg.append("text")
+    .attr("class", "section-title")
+    .attr("x", config.tableStartX + config.tableWidth / 2)
+    .attr("y", config.tableStartY - headerOffset)
+    .text("Table");
+
+  svg.append("text")
+    .attr("class", "section-title")
+    .attr("x", config.storageStartX + config.storageWidth / 2) 
+    .attr("y", config.storageStartY - headerOffset)
+    .text(storage_title);
+}
+
+function writeRowGroupHeaders(svg) {
+  for (let rg = 0; rg < config.numRG; rg++) {
+    svg.append("text")
+      .attr("class", "section-subtitle")
+      .attr("x", config.storageStartX - 50)
+      .attr("y", config.storageStartY + rg * (config.cellSize + config.spacing * 1.5) + 30)
+      .text(`Row Group ${rg}`);
+  }
+
+}
+
+
+function drawGhosts(svg) {
+  // Add ghost table grid (always visible)
+  for (let r = 0; r < config.tableRows; r++) {
+    for (let c = 0; c < config.tableCols; c++) {
+      svg.append("rect")
+        .attr("class", "ghost")
+        .attr("x", config.tableStartX + c * (config.cellSize + config.spacing))
+        .attr("y", config.tableStartY + r * (config.cellSize + config.spacing))
+        .attr("width", config.cellSize)
+        .attr("height", config.cellSize);
+
+      svg.append("text")
+        .attr("class", "ghost")
+        .attr("x", config.tableStartX + c * (config.cellSize + config.spacing) + config.cellSize / 2)
+        .attr("y", config.tableStartY + r * (config.cellSize + config.spacing) + config.cellSize / 2 + 5)
+        .text(`R${r}C${c}`)
+        .attr("text-anchor", "middle");
+    }
+  }
+
+  // Add ghost storage disk (always visible)
+  for (let r=0; r < 2; r++) {
+    svg.append("rect")
+      .attr("class", "ghost")
+      .attr("x", config.storageStartX)
+      .attr("y", config.storageStartY + r * (config.cellSize + config.spacing * 1.5))
+      .attr("width", config.storageWidth)
+      .attr("height", config.cellSize);
+  }
+}
+
+
+function createCells(svg) {
+  const data = [];
+
+  for (let r = 0; r < config.tableRows; r++) {
+    for (let c = 0; c < config.tableCols; c++) {
+      data.push({
+        id: `R${r}C${c}`,
+        row: r,
+        col: c,
+        label: `R${r}C${c}`,
+        color: config.colors[c % config.colors.length]
+      });
+    }
+  }
+
+  const cells = svg.selectAll(".cell")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "cell")
+    .attr("x", d => config.tableStartX + d.col * (config.cellSize + config.spacing))
+    .attr("y", d => config.tableStartY + d.row * (config.cellSize + config.spacing))
+    .attr("width", config.cellSize)
+    .attr("height", config.cellSize)
+    .attr("fill", d => d.color);
+
+  const labels = svg.selectAll(".label")
+    .data(data)
+    .enter()
+    .append("text")
+    .attr("class", "label")
+    .attr("x", d => config.tableStartX + d.col * (config.cellSize + config.spacing) + config.cellSize / 2)
+    .attr("y", d => config.tableStartY + d.row * (config.cellSize + config.spacing) + config.cellSize / 2 + 5)
+    .text(d => d.label)
+    .attr("text-anchor", "middle");
+}
+
+function createRowViz() {
+  const svg = d3.select("#viz-row");
+  writeSectionHeaders(svg, "Row Storage (on disk)");
+  drawGhosts(svg);
+  createCells(svg);
+
+  let inStorageView = false;
+  svg.on("click", () => {
+    inStorageView = !inStorageView;
+    transitionViz("row", inStorageView)
+  })
+}
+
+function createColumnViz() {
+  const svg = d3.select("#viz-column");
+  writeSectionHeaders(svg, "Column Storage (on disk)");
+  drawGhosts(svg);
+  createCells(svg);
+  
+  let inStorageView = false;
+  svg.on("click", () => {
+    inStorageView = !inStorageView;
+    transitionViz("column", inStorageView)
+  })
+}
+
+function createHybridViz() {
+  const svg = d3.select("#viz-hybrid");
+  writeSectionHeaders(svg, "Hybrid Storage");
+  drawGhosts(svg);
+  createCells(svg);
+  writeRowGroupHeaders(svg);
+  
+  let inStorageView = false;
+  svg.on("click", () => {
+    inStorageView = !inStorageView;
+    transitionViz("hybrid", inStorageView)
+  })
+}
+
+function getIndex(d, type) {
+  if (type === "row") return d.row * config.tableCols + d.col;
+  if (type === "column") return d.col * config.tableRows + d.row;
+  if (type === "hybrid") return d.row % config.rowGroupSize + d.col * config.rowGroupSize + Math.floor(d.row / config.rowGroupSize) * config.rowGroupSize * config.tableCols;
+}
+
+function getStorageWrap(type) {
+  if (type === "hybrid") {
+    return config.cellsPerRG;
+  } else {
+    return config.storageWrap;
+  }
+}
+
+function transitionViz(type, toStorage) {
+  const svg = d3.select(`#viz-${type}`)
+  const cells = svg.selectAll(".cell")
+  const labels = svg.selectAll(".label")
+  const duration = 1000
+
+  svg.selectAll(".cell")
+    .transition()
+    .duration(duration)
+    .delay(d => getIndex(d, type) * 50)
+    .attr("x", d => {
+      if (!toStorage) {
+        return config.tableStartX + d.col * (config.cellSize + config.spacing);
+      } else {
+        return config.storageStartX + (getIndex(d, type) % getStorageWrap(type)) * (config.cellSize + config.spacing)
+      }
+    })
+    .attr("y", d => {
+      if (!toStorage) {
+        return config.tableStartY + d.row * (config.cellSize + config.spacing);
+      } else {
+        return config.storageStartY + Math.floor(getIndex(d, type) / getStorageWrap(type)) * (config.cellSize + config.spacing * 1.5);
+      }
+    })
+
+  svg.selectAll(".label")
+    .transition()
+    .duration(duration)
+    .delay(d => getIndex(d, type) * 50)
+    .attr("x", d => {
+      if (!toStorage) {
+        return config.tableStartX + d.col * (config.cellSize + config.spacing) + config.cellSize / 2;
+      } else {
+        return config.storageStartX + (getIndex(d, type) % getStorageWrap(type)) * (config.cellSize + config.spacing) + config.cellSize / 2;
+      }
+    })
+    .attr("y", d => {
+      if (!toStorage) {
+        return config.tableStartY + d.row * (config.cellSize + config.spacing) + config.cellSize / 2 + 5;
+      } else {
+        return config.storageStartY + Math.floor(getIndex(d, type) / getStorageWrap(type)) * (config.cellSize + config.spacing * 1.5) + config.cellSize / 2 + 5;
+      }
+    })
+}
+
+function toggleAllViz() {
+  inStorageView = !inStorageView;
+  transitionViz("row", inStorageView)
+  transitionViz("column", inStorageView)
+  transitionViz("hybrid", inStorageView)
+}
+
+
