a7fbd1c3d67db62088057ade29b1413e09420556
Kishan Patel <kishanpatel789@gmail.com>
2025-08-19 16:35:00 -0500

    Edit presentatioN

--- what_is_a_parquet_file/demo.py
+++ what_is_a_parquet_file/demo.py
@@ -1,53 +1,43 @@
 # %%
-from datetime import datetime
-import pyarrow as pa
+# explore parquet file
 import pyarrow.parquet as pq
-import random
-
-# %%
 
 file_path = "data/yellow_tripdata_2025-05.parquet"
-
-# %%
 table = pq.read_table(file_path)
-table.schema
+
 table.shape
 
-table
+table.schema
 
-# %%
-parquet_file = pq.ParquetFile(file_path)
-parquet_file.metadata
-parquet_file.schema
-
-parquet_file.num_row_groups
-
-parquet_file.read_row_group(0)
-
-parquet_file.read()
-
-
-# %%
-metadata = parquet_file.metadata
-metadata.row_group(0)
-
-for i in range(metadata.num_row_groups):
-    print(f"{i}: {metadata.row_group(i).num_rows}")
-
-metadata.row_group(0).column(0)
+table.slice(0, 5)
 
 # %%
 # save as csv
-df = parquet_file.read().to_pandas()
+df = table.to_pandas()
 
 df.shape
 
 df.to_csv("data/yellow_tripdata_2025-05.csv", index=False)
 
-# du -h data/yellow_tripdata_2025-05*
-# du -h data/*
 
 # %%
+# explore parquet file some more
+parquet_file = pq.ParquetFile(file_path)
+
+metadata = parquet_file.metadata
+
+for i in range(metadata.num_row_groups):
+    print(f"{i}: {metadata.row_group(i).num_rows}")
+
+metadata.row_group(0)
+
+metadata.row_group(0).column(0)
+
+
+# %%
+# plain encoding vs dictionary encoding
+import pyarrow as pa
+import random
 numbers = [random.randint(1, 1000) for _ in range(1_000_000)]
 numbers_table = pa.table({"numbers": numbers})
 
@@ -70,23 +60,25 @@ pq.write_table(
 # dictionary encoding
 pq.write_table(numbers_table, "data/numbers_dict.parquet", use_dictionary=True)
 
+
+
+
+
+
+
+
+
+
+
+
 # %%
 # run length_encoding
 sorted_table = pa.table({"numbers": sorted(numbers)})
+
 sorted_table
 
 pq.write_table(sorted_table, "data/numbers_sorted.parquet")
 
-# %%
-# check plain encoding, dictionary encoding
-plain_file = pq.ParquetFile("data/numbers_plain.parquet")
-dict_file = pq.ParquetFile("data/numbers_dict.parquet")
-sorted_file = pq.ParquetFile("data/numbers_sorted.parquet")
-
-# %%
-plain_file.metadata.row_group(0).column(0)
-dict_file.metadata.row_group(0).column(0)
-sorted_file.metadata.row_group(0).column(0)
 
 
 # %%
