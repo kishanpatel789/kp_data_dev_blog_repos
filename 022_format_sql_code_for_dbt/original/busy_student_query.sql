b1b31b9f28b6704826d8146585e837e06dd36b15
Kishan Patel <kishanpatel789@gmail.com>
2026-04-14 15:53:50 -0500

    Add line breaks

--- 022_format_sql_code_for_dbt/original/busy_student_query.sql
+++ 022_format_sql_code_for_dbt/original/busy_student_query.sql
@@ -1,1 +1,3 @@
-with busy_students as(select s.id,s.name,count(e.class_id) as class_count from students s join enrollments e on s.id=e.student_id group by s.id,s.name having count(e.class_id)>3)select * from busy_students where name like '%Granger%';
+WITH busy_students as(select s.id,s.name,count(e.class_id) as class_count from students 
+s join enrollments e on s.id=e.student_id group by s.id,s.name HAVING count(e.class_id)>3)select * 
+fRom busy_students where name like '%Granger%';
--- 022_format_sql_code_for_dbt/original/dbt_query.sql
+++ 022_format_sql_code_for_dbt/original/dbt_query.sql
@@ -1,1 +1,8 @@
-{{config(materialized='table',tags=['hogwarts','students'],schema='analytics')}}with student_base as(select id,first_name,last_name from {{ref('student')}}),house_lookup as(select id,house_name from {{source('core','house')}}),enrollments as(select student_id,class_id,enrolled_at from {{ref('enrollment')}}),aggregated as(select s.id,s.first_name,s.last_name,h.house_name,count(e.class_id) as class_count,max(e.enrolled_at) as last_enrolled_at from student_base s left join enrollments e on s.id=e.student_id join house_lookup h on s.house_id=h.id group by s.id,s.first_name,s.last_name,h.house_name)select id,first_name,last_name,house_name,class_count,last_enrolled_at from aggregated order by class_count desc
+{{config(materialized='table',tags=['hogwarts','students'],
+schema='analytics')}}with student_base as(select id,first_name,last_name from 
+{{ref('student')}}),house_lookup as(select id,house_name from {{source('core','house')}}),
+enrollments as(select student_id,class_id,enrolled_at from {{ref('enrollment')}}),aggregated as
+(SELECT s.id,s.first_name,s.last_name,h.house_name,count(e.class_id) as class_count,max(e.enrolled_at
+) as last_enrolled_at from student_base s left join enrollments e on s.id=e.student_id join house_lookup 
+h on s.house_id=h.id group by s.id,s.first_name,s.last_name,h.house_name)select id,first_name,last_name
+,house_name,class_count,last_enrolled_at from aggregated order by class_count desc
